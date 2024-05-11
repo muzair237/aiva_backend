decfe0ddfb48cee8d93646833acb3a95c7a62a8a
hafiz danish <danishhabib506@gmail.com>
2024-05-11 02:08:34 -0700

    query controller task done

--- src/controllers/queryController.js
+++ src/controllers/queryController.js
@@ -1,5 +1,4 @@
 import natural from 'natural';
-<<<<<<< HEAD
 import { QnA} from '../models/index.js';
 
 export default {
@@ -27,22 +26,3 @@ export default {
 }
 
   }
-=======
-import { QnA } from '../models/index.js';
-
-export default {
-  askQuery: async (req, res) => {
-    const { query } = req.body;
-
-    if (!query) {
-      return res.status(401).json({ success: false, message: 'Please Enter any Query!' });
-    }
-
-    const tokenizer = new natural.WordTokenizer();
-    const tokens = tokenizer.tokenize(query);
-    const matchingQuestions = await QnA.find({ keywords: { $in: tokens } });
-
-    return res.status(200).json({ success: true, tokens, matchingQuestions });
-  },
-};
->>>>>>> 9e73c4295a0c3aea6fc46aebfa219e10fce6238f
