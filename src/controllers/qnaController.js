2236b0ec8dafd2047ccf20a699c4bf485fc9d225
Uzair <uzair.ejaz2001@gmail.com>
2024-05-14 10:57:35 +0500

    feat: get all feddback api completed

--- src/controllers/qnaController.js
+++ src/controllers/qnaController.js
@@ -1,9 +1,54 @@
 import { QnA } from '../models/index.js';
 import defaultQnA from '../utils/default/q&a.json' assert { type: 'json' };
+import helper from '../utils/helper.js';
 
 export default {
   restoreQnA: async (req, res) => {
     await QnA.insertMany(defaultQnA);
     return res.status(201).json({ success: true, message: "'Questions and Answers Restored Successfully!'" });
+  },
+
+  getAllQuestions: async (req, res) => {
+    const { page, itemsPerPage, startDate, endDate, searchText, sort, type } = {
+      ...req.query,
+      ...helper.filterQuery(req),
+    };
+    const query = {};
+
+    if (type) {
+      query.roles = type;
+    }
+
+    if (searchText) {
+      query.$or = [
+        { question: { $regex: searchText, $options: 'i' } },
+        { answer: { $regex: searchText, $options: 'i' } },
+        { keywords: { $in: [new RegExp(searchText, 'i')] } },
+
+      ];
+    }
+
+    if (startDate && endDate) {
+      const start = new Date(startDate);
+      start.setHours(0, 0, 0, 0);
+      const end = new Date(endDate);
+      end.setHours(23, 59, 59, 999);
+      query.createdAt = { $gte: start, $lt: end };
+    }
+
+    const count = await QnA.countDocuments(query);
+
+    const sorting = helper.getSorting(sort, 'question');
+
+    const admins = await QnA.find(query)
+      .skip((page - 1) * itemsPerPage)
+      .limit(itemsPerPage)
+      .sort(sorting);
+
+    return res.status(200).json({
+      message: 'Questions fetched successfully',
+      success: true,
+      ...helper.pagination(admins, page, count, itemsPerPage),
+    });
   },
 };
--- src/routes/qnaRoutes.js
+++ src/routes/qnaRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { rateLimit } from 'express-rate-limit';
 // import isUser from '../middlewares/isAdmin.js';
+import isAdmin from '../middlewares/isAdmin.js';
 import tryCatch from '../middlewares/tryCatch.js';
 import { QnAController } from '../controllers/index.js';
 import { WINDOW, MAX_LIMIT } from '../../env.js';
@@ -23,5 +24,6 @@ QnARoutes.get(
   ],
   tryCatch(QnAController.restoreQnA),
 );
+QnARoutes.get('/get-all-questions', [limiter, isAdmin], tryCatch(QnAController.getAllQuestions));
 
 export default QnARoutes;
